df5d8e22d9f5cbc7793e96ac0e3e091b5c78e371
Abhimanyu Chaudhary <madrobin1340@gmail.com>
2025-05-15 23:10:55 -0400

    basic frontend works!

---
+++ frontend/frontend.go
@@ -0,0 +1,95 @@
+package frontend
+
+import (
+	"TerminalAI/ai"
+	"fmt"
+
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type (
+	errMsg error
+)
+
+type model struct {
+	textInput textinput.Model
+	err       error
+}
+
+func InitialModel() model {
+	ti := textinput.New()
+	ti.Placeholder = "type here"
+	ti.Focus()
+	ti.CharLimit = 3000
+	ti.Width = 3000
+	ti.Prompt = "user > "
+
+	return model{
+		textInput: ti,
+		err:       nil,
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return textinput.Blink
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
+	switch msg := msg.(type) {
+
+	case tea.KeyMsg:
+
+		switch msg.Type {
+
+		case tea.KeyCtrlC, tea.KeyEsc:
+			return m, tea.Quit
+
+		case tea.KeyEnter:
+			if m.textInput.Value() == "" {
+				return m, nil
+			} else if m.textInput.Value()[0] == '/' {
+				if m.textInput.Value() == "/clear" {
+					fmt.Print("\033[H\033[2J")
+					m.textInput.Reset()
+					return m, nil
+				} else if m.textInput.Value() == "/exit" || m.textInput.Value() == "/quit" || m.textInput.Value() == "/q" || m.textInput.Value() == "/bye" { 
+					return m, tea.Quit
+				} else if m.textInput.Value() == "/help" {
+					m.textInput.Reset()
+					fmt.Println("Available commands:",
+					"\n  - /clear: Clear the screen",
+					"\n  - /exit or /quit or /bye or /q: Exit the program",
+					"\n  - /help: shows commands",
+					"\n",
+					)
+					return m, nil
+			}
+
+			} else {
+				response := ai.MakeRequest(m.textInput.Value())
+				fmt.Println("\n;) > ", response, "\n")
+				m.textInput.Reset()
+			}
+
+		}
+
+	case errMsg:
+		m.err = msg
+		return m, nil
+
+	}
+
+	m.textInput, cmd = m.textInput.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	return fmt.Sprintf(
+		"%s",
+		// "(esc to quit)",
+		m.textInput.View(),
+	) + "\n"
+}
--- main.go
+++ main.go
@@ -2,45 +2,23 @@ package main
 
 import (
 	"TerminalAI/ai"
+	"TerminalAI/frontend"
 	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
 
 	ai.SetUpAPI()
 
-	fmt.Println(ai.MakeRequest("How are you"))
+	// fmt.Println(ai.MakeRequest("How are you"))
 
-
-
-	// logger.SetupLogger()
-
-	// err := godotenv.Load()
-
-	// if err != nil {
-	// 	logger.Logger().Error(err.Error())
-	// }
-
-    // ctx := context.Background()
-    // client, err := genai.NewClient(ctx, &genai.ClientConfig{
-    //     APIKey:  os.Getenv("API_KEY"),
-    //     Backend: genai.BackendGeminiAPI,
-    // })
-
-	// if err != nil {
-	// 	logger.Logger().Error(err.Error())
-	// }
-
-	// result, err := client.Models.GenerateContent(
-    //     ctx,
-    //     "gemini-2.0-flash",
-    //     genai.Text("Make a chocolate chip cookie recipe"),
-    //     nil,
-    // )
-
-	// if err != nil {
-	// 	logger.Logger().Error(err.Error())
-	// }
-
-	// fmt.Println(result.Text())
+	// Initialize the model
+	p := tea.NewProgram(frontend.InitialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Error running program: %v\n", err)
+		os.Exit(1)
+	}
 }
